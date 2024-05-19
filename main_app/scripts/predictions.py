52fc11617e200d1b4cba1de74dd29f7e20508c2c
vitrineofcode <tilers_unable_0n@icloud.com>
2024-05-19 17:23:51 -0400

    Updates to comments

--- main_app/scripts/predictions.py
+++ main_app/scripts/predictions.py
@@ -17,7 +17,7 @@ def preprocess_image(image_path):
     # Converts the image from BGR color space (default in OpenCV) to RGB color space
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     
-    # TODO Check if this works
+    # TODO Check if this works with grayscale images
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # Converts the image to a floating-point tensor with values in the range [0, 255]
