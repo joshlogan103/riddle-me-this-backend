2505508977f6a44b4fd6d22e577c1a7911efe1cd
Josh Logan <joshlogan103@gmail.com>
2024-05-21 09:55:26 -0400

    complete docker implementation

--- startup.sh
+++
@@ -1,8 +0,0 @@
-# Install TensorFlow if not already installed
-if ! python -c "import tensorflow" &> /dev/null; then
-    pip install tensorflow
-fi
-
-# Start the application
-exec gunicorn Riddle_Me_This.wsgi --bind 0.0.0.0:$PORT
-
