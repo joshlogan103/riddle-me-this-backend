1beeca03681445bbcc3a82f349e94ce54f87de88
Josh Logan <joshlogan103@gmail.com>
2024-05-22 12:00:15 -0400

    add simple serializers which can be referenced by the main model serializers to avoid circular references

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -2,37 +2,73 @@ from rest_framework import serializers
 from .models import Profile, ScavengerHunt, HuntInstance, Item, RiddleItem, RiddleItemSubmission, Participation
 from django.contrib.auth.models import User
 
+# Simple Serializers
+
+class SimpleItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = "__all__"
+
+class SimpleScavengerHuntSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ScavengerHunt
+        fields = "__all__"
+        read_only_fields = ('creator',)
+
+class SimpleRiddleItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RiddleItem
+        fields = "__all__"
+
+class SimpleRiddleItemSubmissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RiddleItemSubmission
+        fields = "__all__"
+
+class SimpleParticipationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Participation
+        fields = "__all__"
+
+class SimpleHuntInstanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HuntInstance
+        fields = "__all__"
+
+class SimpleProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = "__all__"
+
+class SimpleUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
+
+# Full Serializers
+
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = "__all__"
 
 class ScavengerHuntSerializer(serializers.ModelSerializer):
-    riddle_items = serializers.SerializerMethodField()
-    hunt_instances = serializers.SerializerMethodField()
+    riddle_items = SimpleRiddleItemSerializer(many=True, read_only=True)
+    hunt_instances = SimpleHuntInstanceSerializer(many=True, read_only=True)
 
     class Meta:
         model = ScavengerHunt
         fields = "__all__"
         read_only_fields = ('creator',)
 
-    def get_riddle_items(self, obj):
-        return RiddleItemSerializer(obj.riddleitem_set.all(), many=True).data
-
-    def get_hunt_instances(self, obj):
-        return HuntInstanceSerializer(obj.huntinstance_set.all(), many=True).data
-
 class RiddleItemSerializer(serializers.ModelSerializer):
     item = ItemSerializer(read_only=True)
-    scavenger_hunt = ScavengerHuntSerializer(read_only=True)
-    riddle_item_submissions = serializers.SerializerMethodField()
+    scavenger_hunt = SimpleScavengerHuntSerializer(read_only=True)
+    riddle_item_submissions = SimpleRiddleItemSubmissionSerializer(many=True, read_only=True)
 
     class Meta:
         model = RiddleItem
         fields = "__all__"
-
-    def get_riddle_item_submissions(self, obj):
-        return RiddleItemSubmissionSerializer(obj.riddleitemsubmission_set.all(), many=True).data
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -50,34 +86,34 @@ class UserSerializer(serializers.ModelSerializer):
         return user
 
 class RiddleItemSubmissionSerializer(serializers.ModelSerializer):
-    riddle_item = RiddleItemSerializer(read_only=True)
-    participation = serializers.PrimaryKeyRelatedField(read_only=True)
+    riddle_item = SimpleRiddleItemSerializer(read_only=True)
+    participation = SimpleParticipationSerializer(read_only=True)
 
     class Meta:
         model = RiddleItemSubmission
         fields = "__all__"
 
 class ParticipationSerializer(serializers.ModelSerializer):
-    profile = serializers.PrimaryKeyRelatedField(read_only=True)
-    hunt_instance = serializers.PrimaryKeyRelatedField(read_only=True)
-    riddle_item_submissions = RiddleItemSubmissionSerializer(many=True, read_only=True)
+    profile = SimpleProfileSerializer(read_only=True)
+    hunt_instance = SimpleHuntInstanceSerializer(read_only=True)
+    riddle_item_submissions = SimpleRiddleItemSubmissionSerializer(many=True, read_only=True)
 
     class Meta:
         model = Participation
         fields = "__all__"
 
 class HuntInstanceSerializer(serializers.ModelSerializer):
-    scavenger_hunt = ScavengerHuntSerializer(read_only=True)
-    participations = ParticipationSerializer(many=True, read_only=True)
+    scavenger_hunt = SimpleScavengerHuntSerializer(read_only=True)
+    participations = SimpleParticipationSerializer(many=True, read_only=True)
 
     class Meta:
         model = HuntInstance
         fields = "__all__"
 
 class ProfileSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
-    hunt_instances = HuntInstanceSerializer(many=True, read_only=True)
-    participations = ParticipationSerializer(many=True, read_only=True)
+    user = SimpleUserSerializer(read_only=True)
+    hunt_instances = SimpleHuntInstanceSerializer(many=True, read_only=True)
+    participations = SimpleParticipationSerializer(many=True, read_only=True)
 
     class Meta:
         model = Profile
